7c6f4b51d9ccb5450f39e3425e2bdfc03062b4a5
KulchytskyiOleh <44035167+KulchytskyiOleh@users.noreply.github.com>
2018-10-10 23:17:46 +0300

    Add files via upload

---
+++ lab_1.py
@@ -0,0 +1,15 @@
+cours ='Створення прикладних програм на мові Python'
+lab = 'Лабораторна №1'
+print (cours, lab, sep = ', ')
+
+name = 'Oleg Kulchytskyi'
+zalikov = '№ 2348'
+print ( name + ' ' , zalikov, sep = ', ')
+
+vukl = 'Ivan Savka '
+prob =', '
+result = ((vukl+prob)*44+vukl)
+print (result)
+
+x = 11.2/(11.2-211.3) - (15.2+11.3)/(123.1)
+print('x = ', x)
